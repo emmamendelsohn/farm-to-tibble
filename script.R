685907007aaffebaed81f0dc60e23b2419cc82e4
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-01-20 21:34:41 -0500

    debugging

--- script.R
+++ script.R
@@ -9,13 +9,13 @@ page <- xml2::read_html(url)
 flowers <- xml2::xml_find_all(page, xpath="//a[@class='c-navigation-block__link refinement-link']") %>%
   xml2::xml_attrs() %>%
   map(~.x[["href"]])
-
+flowers <- flowers[c(-37, -75)]
 
 #get varieties
 df <- map_df(flowers, function(flower){
   page <- xml2::read_html(flower)
   
-  flower_name <- xml2::xml_find_all(page, xpath="//h1[@class='c-navigation-block__header refinement-header']") %>%
+  flower_name <- xml2::xml_find_all(page, xpath="//div[@class='c-slider__header']") %>%
     xml_text() %>%
     str_remove_all("\n")
   
@@ -23,37 +23,50 @@ df <- map_df(flowers, function(flower){
     xml2::xml_attrs() %>%
     map(~paste0("https://www.johnnyseeds.com", .x[["href"]]))
   
-  map_df(vars, function(var){
-      page <- xml2::read_html(var) 
-      
-      var_name <- xml_find_all(page, xpath="//h1[@class='c-product-header__heading product-name']") %>%
+    map_df(vars, function(var){
+    page <- xml2::read_html(var) 
+    
+    var_name <- xml_find_all(page, xpath="//h1[@class='c-product-header__heading product-name']") %>%
+      xml_text() %>%
+      str_remove_all("\n")
+    
+    product_id <- xml_find_all(page, xpath="//span[@itemprop='productID']") %>%
+      xml_text()
+    
+    flower_nodeset <- xml_find_all(page, xpath="//div[@class='c-accordion__body s-lgc-pdp-content js-accordion__body']") %>%
+      xml_children()
+    
+    headers_nodeset <- xml_find_all(flower_nodeset, xpath ="//span[contains(@style, 'bold')]")
+
+    index <- seq_along(flower_nodeset)
+    headers_index <- which(flower_nodeset %in% headers_nodeset)
+    dummy_index <- c(headers_index[-1], length(flower_nodeset)+1)
+    content_index <- map2(headers_index, dummy_index, function(x,y){
+      index[index > x & index < y]
+    })
+    
+    headers <- flower_nodeset[headers_index] %>% xml_text()
+    content <- map_dfc(content_index, function(x){
+      out <- flower_nodeset[x] %>% 
         xml_text() %>%
-        str_remove_all("\n")
-
-      product_id <- xml_find_all(page, xpath="//span[@itemprop='productID']") %>%
-        xml_text()
-      
-      flower_nodeset <- xml_find_all(page, xpath="//div[@class='c-accordion__body s-lgc-pdp-content js-accordion__body']") %>%
-        xml_children()
-
-      headers_nodeset <- xml_find_all(flower_nodeset, xpath = "//span[@style='font-weight: bold;']")
-      headers_index <- which(flower_nodeset %in% headers_nodeset)
-      content_index <- which(!flower_nodeset %in% headers_nodeset)
-      
-      headers <- flower_nodeset[headers_index] %>% xml_text()
-      content <- flower_nodeset[content_index] %>% xml_text()
-      
-      assertthat::are_equal(length(headers), length(content))
-     
-      m <- matrix(content, nrow = 1)
-      colnames(m) <- headers
-      as_tibble(m) %>%
-        mutate(flower = flower_name, variety = var_name, product_id = product_id) %>%
-        janitor::clean_names()  
+        map(~trimws(.x)) %>%
+        discard(~.x=="") %>%
+        unlist()
+      if(length(out)>1) {out <- reduce(out,paste)}
+      if(is.null(out)){out <- ""}
+      return(out)
+    })
+    
+    assertthat::are_equal(length(headers), ncol(content))
+    headers_clean <- janitor::make_clean_names(headers)
+    
+    content %>%
+      set_names(headers_clean) %>%
+      mutate(flower = flower_name, variety = var_name, product_id = product_id)
   })
   
 })
-    
+
 df2 <- df %>%
   select(product_id, flower, variety, everything())
 
